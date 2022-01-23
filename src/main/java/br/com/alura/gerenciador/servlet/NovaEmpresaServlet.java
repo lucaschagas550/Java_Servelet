c948d0fc7ac2d0cf19c042752952702a294d4185
Lucas Andrade <lucasandrade595@gmail.com>
2022-01-23 19:28:16 -0300

    Feat: Definindo o modelo.

--- src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -14,10 +14,16 @@ public class NovaEmpresaServlet extends HttpServlet {
 
 	
 	//doPost so aceita post na requisicao doGet ...
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String nomeEmpresa = request.getParameter("nome");
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
 		System.out.println("cadastrando nova empresa.");
+		
+        String nomeEmpresa = request.getParameter("nome");
+        Empresa empresa = new Empresa();
+        empresa.setNome(nomeEmpresa);
+        
+        Banco banco = new Banco();
+        banco.adiciona(empresa);
+		
 		PrintWriter out = response.getWriter();
 		out.println("<html><body>Empresa "+ nomeEmpresa +" cadastrada com sucesso.</body></html");
 	}
