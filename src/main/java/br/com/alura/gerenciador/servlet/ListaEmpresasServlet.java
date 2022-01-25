15fadbe75bead1567dc28e5eb62e0f20baeb902d
Lucas Andrade <lucasandrade595@gmail.com>
2022-01-25 18:47:56 -0300

    Feat: Lista de empresas com servlet.

---
+++ src/main/java/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
@@ -0,0 +1,35 @@
+package br.com.alura.gerenciador.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet(urlPatterns = "/listaEmpresas")
+public class ListaEmpresasServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public ListaEmpresasServlet() {
+        super();
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Banco banco = new Banco();
+		List<Empresa> lista = banco.getEmpresas();
+		
+		PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<ul>");
+        for (Empresa empresa : lista) { 
+            out.println("<li>" + empresa.getNome() + "</li>");
+        }        
+        out.println("</ul>");
+        out.println("</body></html>");
+	}
+
+}
